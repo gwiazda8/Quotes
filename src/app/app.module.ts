231a6dcafe1568075ef66a93c30255839445239b
Damian <ibra8@wp.pl>
2019-02-19 11:54:14 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
+import { LoginComponent } from './login/login/login.component';
+import { RegisterComponent } from './login/register/register.component';
+import { InviteComponent } from './project/invite/invite.component';
+import { NewProjectComponent } from './project/new-project/new-project.component';
+import { ProjectItemComponent } from './project/project-item/project-item.component';
+import { ProjectListComponent } from './project/project-list/project-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    InviteComponent,
+    NewProjectComponent,
+    ProjectItemComponent,
+    ProjectListComponent
   ],
   imports: [
     BrowserModule
